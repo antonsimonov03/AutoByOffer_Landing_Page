4b4db48e3f22e3874f35410d80efa3092f11a1c4
Anton <antonsimonov03@gmail.com>
2018-05-29 16:52:58 +0300

    all files commited.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './home/menu/menu.component';
 import { SearchComponent } from './home/search/search.component';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import {NouisliderModule} from 'ng2-nouislider';
+import { UnitsComponent } from './units/units.component';
+import { AllUnitsComponent } from './all-units/all-units.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuComponent,
-    SearchComponent
+    SearchComponent,
+    UnitsComponent,
+    AllUnitsComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule,
+    NouisliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  range = [35, 80];
+  sliderConfig: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.sliderConfig = {
+      behaviour: 'drag',
+      connect: true,
+      range: {
+        min: 0,
+        max: 100
+      },
+      tooltips: [
+        {
+          to (value: number) {
+            return value.toFixed(0);
+          }
+        },
+        {
+          to (value: number) {
+            return value.toFixed(0);
+          }
+        }
+      ]
+    };
   }
 
 }
